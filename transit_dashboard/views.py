dac4efbdbdc0329bc5cbc3f3fa44150317740934
Mark Peng <peng.markv@gmail.com>
2016-08-06 23:56:10 -0700

    Add gmaps query

--- transit_dashboard/views.py
+++ transit_dashboard/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+import googlemaps
+from datetime import datetime
+import os
+
+gmaps = googlemaps.Client(key=os.environ.get('TRANSITDASH_GOOGLE_SECRET'))
 
 def index(request):
-    return render(request, 'index.html')
+    result = gmaps.distance_matrix("542 Green St, San Francisco, CA 94133", 
+                                         "700 4th St, San Francisco, CA 94107",
+                                         mode="transit",
+                                         departure_time=datetime.now())
+    return render(request, 'index.html', {'result': result})
